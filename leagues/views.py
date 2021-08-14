e6373b8412ff26e52fa44ecbd78e57660d003704
gonzalo <gonzaloibarra8@gmail.com>
2021-08-14 12:14:55 -0400

    subida archivos

---
+++ leagues/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render, redirect
+from .models import League, Team, Player
+
+from . import team_maker
+
+def index(request):
+	context = {
+		"leagues": League.objects.all(),
+		"teams": Team.objects.all(),
+		"players": Player.objects.all(),
+		"leagues_mujeres": League.objects.filter(name__icontains='Womens'),
+		"leagues_hockey": League.objects.filter(name__icontains='Hockey'),
+		"leagues_no_football": League.objects.exclude(sport__icontains='football'),
+		"leagues_no_name_conference": League.objects.filter(name__icontains='Conference'),
+		"leagues_region_atlantida":League.objects.filter(name__icontains='atlantic'),
+		"teams_dallas": Team.objects.filter(location__icontains='Dallas'),
+		"teams_raptors": Team.objects.filter(team_name__icontains='raptors'),
+		"teams_city": Team.objects.filter(location__icontains='city'),
+		"teams_start": Team.objects.filter(team_name__startswith='T'),
+		"teams_alfa": Team.objects.order_by("location"),
+		"teams_inv": Team.objects.order_by('-team_name'),
+		"players_cooper": Player.objects.filter(last_name__contains='Cooper'),
+		"players_joshua": Player.objects.filter(first_name__contains='Joshua'),
+		"players_cooper_ex": Player.objects.filter(last_name__contains='Cooper').exclude(first_name__contains="Joshua"),
+		"players_alex_wyatt": Player.objects.filter(first_name__contains='Alexander') | Player.objects.filter(first_name__contains='Wyatt'),
+
+		"teams_atlantic_soccer":Team.objects.filter(league__name__contains='Atlantic Soccer Conference'),
+		"player_penguins":Player.objects.filter(curr_team__team_name__contains='Penguins'),
+		"player_international":Player.objects.filter(curr_team__league__name__contains='International Collegiate Baseball Conference'),
+		"player_american":Player.objects.filter(curr_team__league__name__contains='American Conference of Amateur Football') & Player.objects.filter(last_name__contains='Lopez'),
+		"player_futbol":Player.objects.filter(curr_team__league__sport__contains='football'),
+		"teams_sophia":Team.objects.filter(curr_players__first_name__contains='Sophia'),
+		"league_sophia":League.objects.filter(teams__curr_players__first_name__contains='Sophia'),
+		"player_Flores":Player.objects.filter(last_name__contains='flores') & Player.objects.exclude(curr_team__team_name='Roughriders'),
+
+
+
+
+
+	}
+	return render(request, "leagues/index.html", context)
+
+def make_data(request):
+	team_maker.gen_leagues(10)
+	team_maker.gen_teams(50)
+	team_maker.gen_players(200)
+
+	return redirect("/")
